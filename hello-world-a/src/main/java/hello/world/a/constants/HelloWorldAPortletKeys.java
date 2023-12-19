c45581355a2e45319375583122fb1e698dae617f
Gabriel Compri <gabriel.compri@liferay.com>
2023-12-19 12:34:58 -0300

    First Module

---
+++ hello-world-a/src/main/java/hello/world/a/constants/HelloWorldAPortletKeys.java
@@ -0,0 +1,11 @@
+package hello.world.a.constants;
+
+/**
+ * @author gabriel
+ */
+public class HelloWorldAPortletKeys {
+
+	public static final String HELLOWORLDA =
+		"hello_world_a_HelloWorldAPortlet";
+
+}
---
+++ hello-world-a/src/main/java/hello/world/a/portlet/HelloWorldAPortlet.java
@@ -0,0 +1,29 @@
+package hello.world.a.portlet;
+
+import hello.world.a.constants.HelloWorldAPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author gabriel
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=HelloWorldA",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + HelloWorldAPortletKeys.HELLOWORLDA,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class HelloWorldAPortlet extends MVCPortlet {
+}
