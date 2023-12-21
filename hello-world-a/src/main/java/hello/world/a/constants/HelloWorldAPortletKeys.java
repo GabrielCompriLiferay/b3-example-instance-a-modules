edebe1cf191eff5480e9bdcfb847f093e6fe7c70
GabrielCompriLiferay <101733505+GabrielCompriLiferay@users.noreply.github.com>
2023-12-21 10:27:04 -0300

    Update HelloWorldAPortletKeys.java

--- hello-world-a/src/main/java/hello/world/a/constants/HelloWorldAPortletKeys.java
+++ hello-world-a/src/main/java/hello/world/a/constants/HelloWorldAPortletKeys.java
@@ -4,7 +4,7 @@ package hello.world.a.constants;
  * @author gabriel
  */
 public class HelloWorldAPortletKeys {
-
+ 
 	public static final String HELLOWORLDA =
 		"hello_world_a_HelloWorldAPortlet";
 
